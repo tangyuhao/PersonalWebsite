f227f0d08eaa7dc4cbf937d10919d5e01fbbbcbe
Yuhao Tang <tangyh@umich.edu>
2017-03-01 01:05:13 -0500

    add google search for my blog

--- controllers/main.py
+++ controllers/main.py
@@ -1,6 +1,6 @@
 from flask import *
 
-main = Blueprint('main', __name__, template_folder='templates')
+main = Blueprint('main', __name__, template_folder='templates', static_folder='../assets/static_html')
 @main.route('/') 
 @main.route('/<args>')
 def show_homepage(args=None):
@@ -10,7 +10,11 @@ def show_homepage(args=None):
         }
         return render_template("home.html",**options)
     elif (".html" in args):
-        abort(403)
+        if (args == "googlec31f2736b5a4f8ab.html"):
+            print(url_for('main.static',filename='test.css'))
+            return main.send_static_file("googlec31f2736b5a4f8ab.html")
+        else:
+            abort(403)
     else:
         abort(404)
 @main.errorhandler(404)
